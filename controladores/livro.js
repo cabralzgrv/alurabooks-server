389090f3cc162b56352f20e299d4e6f06eabbba1
Lucas Cabral <cabralz18@outlook.com>
2023-09-04 22:37:11 -0300

    JSON commit

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,6 +1,9 @@
+const { getTodosLivros } = require("../servicos/livros")
+
 function getLivros(req, res) {
     try {
-        res.send("Hello World!!!")
+        const livros = getTodosLivros()
+        res.send(livros)
     } catch (error) {
         res.status(500)
         res.send(error.message)
